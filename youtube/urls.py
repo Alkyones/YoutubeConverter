a507306e6a23edb2fd81daa5d8439338e9dff358
Atakan Yildirim <lifeonshort@gmail.com>
2025-05-12 22:12:42 +0300

    Implement task status API and update status page to fetch and display task information dynamically

--- youtube/urls.py
+++ youtube/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('status/', views.status, name='status'),
+    path('status/api/', views.get_task_status, name='get_task_status'),
 ]
