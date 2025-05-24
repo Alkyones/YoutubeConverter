9cc4b8ca821a39a4f37a4575f4743bf66b8feeca
Atakan Yildirim <lifeonshort@gmail.com>
2025-05-24 19:09:57 +0300

    Refactor user forms and remove unused imports; clean up database settings and migration files

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate
 
 
 class UserCreationForm(BaseUserCreationForm):
